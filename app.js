61bc121d529b1a3c3159de16ef9017ad97a248d2
ShaanWashimNaiyar <140151457+ShaanWashimNaiyar@users.noreply.github.com>
2023-09-12 01:48:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,11 +73,10 @@ function showWeatherReport(weather){
     
         document.body.style.backgroundImage = "url('images/thunderstorm.png')";
         
-    } 
-         else if(weatherType.textContent == 'Mist') {
+    } else if(weatherType.textContent == 'Mist') {
 
         document.body.style.backgroundImage = "url('images/haze.jpeg')";
-}
+    }
 }
 
 // Date manage
